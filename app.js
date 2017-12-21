4e11eeaf2fcd6f0e041c86d050965ee2b994713a
cwong2015 <sirhc8002@gmail.com>
2017-12-21 10:29:08 -0500

    followed the MVC design pattern

---
+++ app.js
@@ -0,0 +1,14 @@
+// in this app, i will follow the MVC design pattern
+// this is the model
+
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+todoController(app);
+
+app.listen(3000);
