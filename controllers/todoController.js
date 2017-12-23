dd4e2832013c40e028004c215242c484d4215187
cwong2015 <sirhc8002@gmail.com>
2017-12-23 00:48:11 -0500

    more mongoDB stuff, specifically, how to set up schema

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var data = [{item: 'learn node.js'}];
 var urlEncodedParser = bodyParser.urlencoded({extended: 'false'});
+
+mongoose.connect("mongodb://test:test@ds131137.mlab.com:31137/todo")
+
+var schema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', schema);
+var itemOne = Todo({item: "learn react.js"}).save(function(err){
+    if(err) throw err;
+    console.log("item saved");
+});
 
 module.exports = function(app){
     app.get('/todo', function(req, res){
@@ -12,7 +25,10 @@ module.exports = function(app){
         res.json(data);
     });
 
-    app.delete('/todo', function(req, res){
-
+    app.delete('/todo/:item', function(req, res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 }
